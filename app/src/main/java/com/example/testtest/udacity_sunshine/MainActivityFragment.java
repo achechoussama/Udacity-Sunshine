a981104c0b9121ef4ba7d400d01edb361549a8ea
Oussama ACHECHE <achech.oussama@gmail.com>
2016-07-12 11:17:07 +0100

    modified the app to use a fragment

---
+++ app/src/main/java/com/example/testtest/udacity_sunshine/MainActivityFragment.java
@@ -0,0 +1,22 @@
+package com.example.testtest.udacity_sunshine;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by testtest on 7/12/16.
+ * main fragment of the MainActivity
+ */
+public class MainActivityFragment extends Fragment{
+    public MainActivityFragment() {
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+}
